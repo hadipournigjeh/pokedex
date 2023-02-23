75787b88960daf3441ecbbcbf4489c2e5fd61f8e
hadipournigjeh <108987953+hadipournigjeh@users.noreply.github.com>
2023-02-23 23:30:07 +0000

    I've done second part of pokedex

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,26 @@
-import logo from "./logo.svg";
-import "./App.css";
+import "./App.css"
 
 function App() {
-  return <div className="App">
-    <h1>Welcome to the Pokedex</h1>
-  </div>;
+  return (
+    <div>
+      <Logo />
+      <BestPokemon />
+  </div>
+  )
+}
+
+const Logo = () => {
+  return <header> 
+            <div className="App">
+             <h1>Welcome to the Pokedex</h1>
+             <img src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/016.png" alt="pokedex" />
+            </div>
+          </header>
+          
+}
+
+const BestPokemon = () => {
+  return <p>My favorite Pokemon is Squirtle</p>
 }
 
 export default App;
